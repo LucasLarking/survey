988899823250a3395196f16dfee616393b3a69e1
Lucas Larking <lucas.larking@gmail.com>
2023-07-23 16:09:11 +0200

    Improved Styling Of Confirmation Page

--- frontend/src/pages/ConfirmationPage.tsx
+++ frontend/src/pages/ConfirmationPage.tsx
@@ -1,8 +1,21 @@
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ConfirmationPage = () => {
+    const navigate = useNavigate()
     return (
         <>
-            <div>ConfirmationPage</div>
+            <Box sx={{ bgcolor: 'primary.main', p: 10, display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '1000px', margin:'auto', mt: 15 }}>
+                <Typography variant='h3' color={'secondary.main'}>Thank You For Taking The Survey</Typography>
+                <Button onClick={(e) => {navigate('/') }} sx={{
+                    mt: 5,
+                    px: 5,
+                    color: 'black', bgcolor: 'secondary.main', '&:hover': {
+                        bgcolor: 'secondary.dark',
+                        color: 'white'
+                    },
+                }} variant='contained'>Go To Home Page</Button>
+            </Box>
         </>
     )
 }
